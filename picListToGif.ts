9bcb6b3a3112454a4f616a5d8b9177ab440e8a98
jimao <shihao.gu@perfma.com>
2024-10-28 20:11:27 +0800

    base64支持

--- picListToGif.ts
+++ picListToGif.ts
@@ -2,13 +2,13 @@ import { Jimp } from 'jimp';
 import { GifFrame, GifCodec } from 'gifwrap';
 
 export default
-async function picListToGif(pics: Buffer[], delayCentisecs = 20, w = 100, h = 100) {
+async function picListToGif(pics: string[], delayCentisecs = 20, w = 100, h = 100) {
   const frames = await Promise.all(pics.map(async (pic) => {
-    const jip = await Jimp.read(pic);
+    const jip = await Jimp.read(Buffer.from(pic, 'base64'))
     jip.resize({ w, h });
     jip.quantize({ colors: 256 });
     return new GifFrame(jip.bitmap, { delayCentisecs });
   }));
   const gif = await new GifCodec().encodeGif(frames, { loops: 0 });
-  return gif.buffer;
+  return gif.buffer.toString('base64');
 }
