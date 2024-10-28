efc3dbe9e5d2439d98a1474df2a41f29130f54dd
jimao <shihao.gu@perfma.com>
2024-10-28 18:24:35 +0800

    dayjs支持

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
+import dayjs from 'dayjs';
 
 export
 const handler = async (event: any) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
+    timeNow: dayjs().format('YYYY-MM-DD HH:mm:ss'),
   };
   return response;
 };
